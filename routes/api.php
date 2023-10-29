9ef3392fafd6b07d0301d1a29a7bbe1d15aecdc3
joaoLucas8760 <canalluka8760@gmail.com>
2023-10-28 23:17:12 -0300

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+Route::patch('/users/{id}', [UserController::class, 'update']);
+Route::get("/users/{id}", [UserController::class, 'show']);
+Route::get("/users", [UserController::class, 'index']);
+Route::post("/users", [UserController::class, 'store']);
+
+
+Route::get('/', function (){
+    return response()->json(['success' => true]);
+});
+
+
